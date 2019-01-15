01e50ffb055a29988bee16419a2a7607d95c93e0
hong-revo <hongooi@microsoft.com>
2019-01-15 14:30:07 +1100

    test fixing

--- tests/testthat/test01_database.R
+++ tests/testthat/test01_database.R
@@ -12,30 +12,30 @@ srvname <- Sys.getenv("AZ_TEST_KUSTO")
 if(srvname == "")
     skip("Database endpoint tests skipped: resource name not set")
 
-username <- Sys.getenv("AZ_TEST_KUSTO_USER")
-if(username = "")
+username <- Sys.getenv("AZ_TEST_KUSTO_USERNAME")
+if(username == "")
     skip("Database tests skipped, username not set")
 
 
 rgname <- Sys.getenv("AZ_TEST_RG")
 rg <- AzureRMR::az_rm$
-        new(tenant=tenant, app=app, password=password)$
-        get_subscription(subscription)$
-        get_resource_group(rgname)
+    new(tenant=tenant, app=app, password=password)$
+    get_subscription(subscription)$
+    get_resource_group(rgname)
 
 
+dbname <- paste(sample(letters, 10, replace=TRUE), collapse="")
 
 test_that("Database resource functions work",
 {
     srv <- rg$get_kusto_cluster(srvname))
     expect_true(is_kusto_cluster(srv))
 
-    dbname <- paste(sample(letters, 10, replace=TRUE), collapse="")
     db <- srv$create_database(dbname, retention_period=1000, cache_period=30)
     expect_true(is_kusto_database(db))
 
     dbs <- srv$list_databases()
-    expect_true(is.list(dbs) && all(sapply(dbs, is_kusto_database))))
+    expect_true(is.list(dbs) && all(sapply(dbs, is_kusto_database)))
 })
 
 
@@ -43,4 +43,23 @@ test_that("Database endpoint functions work",
 {
     srv <- rg$get_kusto_cluster(srvname))
     expect_true(is_kusto_cluster(srv))
+
+    db <- srv$get_database(dbname)
+    endp1 <- db$get_query_endpoint()
+
+    server <- srv$properties$queryUri
+    endp2 <- kusto_query_endpoint(server=server, database=dbname, tenant=tenant)
+
+    endp3 <- kusto_query_endpoint(server=server, database=dbname,
+        .azure_token=get_kusto_token(srvname, "australiasoutheast", tenant))
+
+    conn_str <- sprintf("server=%s;database=%s;tenant=%s", srvname, dbname, tenant)
+    endp4 <- kusto_query_endpoint(conn_str)
+
+    expect_identical(endp1$token$hash(), endp2$token$hash())
+    expect_identical(endp1$token$hash(), endp3$token$hash())
+    expect_identical(endp1$token$hash(), endp4$token$hash())
 })
+
+
+rg$delete(confirm=FALSE)
