2dde08f1741567b511f0b1dcc52375bb12e46871
hong-revo <hongooi@microsoft.com>
2019-02-20 11:44:55 +1100

    don't collect twice

--- tests/testthat/test06_dplyr.R
+++ tests/testthat/test06_dplyr.R
@@ -22,49 +22,39 @@ db <- kusto_database_endpoint(server=server, database=dbname, tenantid=tenant,
 
 ir <- tbl_kusto(db, "iris")
 
-expect_tibble <- function(x)
-{
-    expect_is(dplyr::collect(x), "tbl_df")
-}
-
 
 test_that("collect works",
 {
-    expect_tibble(ir)
+    expect_is(dplyr::collect(ir), "tbl_df")
 })
 
 test_that("select works",
 {
     out <- dplyr::select(ir, sepal_length, sepal_width)
-    expect_tibble(out)
     expect_identical(ncol(dplyr::collect(out)), 2L)
 })
 
 test_that("distinct works",
 {
     out <- dplyr::distinct(ir, species)
-    expect_tibble(out)
     expect_identical(length(dplyr::collect(out)$species), 3L)
 })
 
 test_that("filter works",
 {
     out <- dplyr::filter(ir, species == "setosa")
-    expect_tibble(out)
     expect_true(all(dplyr::collect(out)$species == "setosa"))
 })
 
 test_that("filter and distinct work",
 {
     out <- dplyr::filter(dplyr::distinct(ir, species), species == "setosa")
-    expect_tibble(out)
     expect_identical(nrow(dplyr::collect(out)), 1L)
 })
 
 test_that("mutate works",
 {
     out <- dplyr::mutate(ir, sl2=sepal_length*2)
-    expect_tibble(out)
     expect_true({
         out <- dplyr::collect(out)
         all_equal(out$sl2, out$sepal_length*2)
@@ -74,7 +64,6 @@ test_that("mutate works",
 test_that("arrange works",
 {
     out <- dplyr::arrange(ir, sepal_length)
-    expect_tibble(out)
     expect_true({
         out <- dplyr::collect(out)
         all(out$sepal_length[-1] >= out$sepal_length[-nrow(out)])
@@ -84,18 +73,16 @@ test_that("arrange works",
 test_that("summarise works",
 {
     out <- dplyr::summarise(ir, m=mean(sepal_length, na.rm=TRUE))
-    expect_tibble(out)
     expect_identical(nrow(dplyr::collect(out)), 1L)
 
     out <- dplyr::summarise(dplyr::group_by(ir, species), m=mean(sepal_length, na.rm=TRUE))
-    expect_tibble(out)
     expect_identical(nrow(dplyr::collect(out)), 3L)
 })
 
 test_that("rename works",
 {
     out <- dplyr::rename(ir, sl=sepal_length)
-    expect_tibble(out)
+    expect_identical(names(dplyr::collect(out))[1], "sl")
 })
 
 
