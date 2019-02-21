7597951b3cc63b3ba31652b6aac69efbb7babdc8
hong-revo <hongooi@microsoft.com>
2019-02-21 20:24:04 +1100

    add missing ... arg

--- R/kusto_token.R
+++ R/kusto_token.R
@@ -71,7 +71,7 @@ delete_kusto_token <- function(server=NULL, clustername, location=NULL, tenant=N
     if(is.null(auth_type) && app == .kusto_app_id && (!"username" %in% names(list(...))))
         auth_type <- "device_code"
 
-    AzureAuth::delete_azure_token(server, tenant, app, auth_type=auth_type, confirm=confirm)
+    AzureAuth::delete_azure_token(server, tenant, app, auth_type=auth_type, confirm=confirm, ...)
 }
 
 
